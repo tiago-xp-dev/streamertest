6a0bb5ad3874636d50049aa161a8593c254fd86e
Tiago <tiagoxp.desenvolvedor@gmail.com>
2021-03-23 21:18:03 -0300

    Atualizada versão do dotnet, nugets e startup.cs migrado.

--- Startup.cs
+++ Startup.cs
@@ -15,7 +15,7 @@ namespace SS_API
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IWebHostEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -29,19 +29,16 @@ namespace SS_API
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<StreamerContext>(
-                x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
-            );
-            services.AddMvc();
+            services.AddControllers();
 
             // Configurando o serviço de documentação do Swagger
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1",
+                c.SwaggerDoc("v2",
                     new OpenApiInfo
                     {
                         Title = "SS Test",
-                        Version = "v1",
+                        Version = "v2",
                         Description = "Streamer - Aplicação de teste",
                         Contact = new OpenApiContact
                         {
@@ -58,23 +55,34 @@ namespace SS_API
 
                 c.IncludeXmlComments(caminhoXmlDoc);
             });
+
+            services.AddDbContext<StreamerContext>(
+                x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
+            );
         }
 
         public void Configure(
             IApplicationBuilder app,
-            IHostingEnvironment env,
+            IWebHostEnvironment env,
             ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory = LoggerFactory.Create(
+                builder => builder.AddConfiguration(Configuration.GetSection("Logging"))
+                .AddConsole()
+                .AddDebug());
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
             // Ativando middlewares para uso do Swagger
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                c.SwaggerEndpoint("/swagger/v2/swagger.json",
                     "Streamer - Aplicação de teste");
                 c.RoutePrefix = string.Empty;
             });
